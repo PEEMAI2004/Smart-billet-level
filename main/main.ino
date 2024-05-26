a21410decc996143ba05454937780919d4d33c79
PEEMAI2004 <56111465+PEEMAI2004@users.noreply.github.com>
2024-05-26 14:54:54 +0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,13 @@
 #include "I2Cdev.h"
 #include "Wire.h"
+#include "display.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+
+int orentation = 2;
+int buttonstate;
 
 // MPU6050 at 60 (0x3C)
 Adafruit_MPU6050 mpu;
@@ -21,7 +25,7 @@ const int buzzerPin = D0;
 // int buttonState = 0;
 
 // Configs
-int diff = 10;
+int diff = 1;
 
 bool isLevelAxis(int val) {
   if (val - diff <= 0 || val + diff >= 45) {
@@ -30,44 +34,44 @@ bool isLevelAxis(int val) {
   return false;
 }
 
-bool isLevel(int valx, int valy, int valz) {
-  valx = valx % 45;
-  valy = valy % 45;
-  valz = valz % 45;
+bool isLevel(int roll, int pitch, int yall) {
+  roll = roll % 90;
+  pitch = pitch % 90;
+  yall = yall % 90;
 
-  if (isLevelAxis(valx) && isLevelAxis(valy) && isLevelAxis(valz)) {
-    Serial.println(" Level");
-    tone(buzzerPin, 4000);
+  if (isLevelAxis(roll) && isLevelAxis(pitch) && isLevelAxis(yall)) {
+    // Serial.println(" Level");
     return true;
   } else {
-    Serial.println(" Not Level");
-    tone(buzzerPin, 6000);
+    // Serial.println(" Not Level");
     return false;
   }
 }
 
-void DisplayLandscape(int valx, int valy, int valz, bool flip) {
+void DisplayLandscape(float roll, float pitch, float yall, bool flip) {
   if (flip) {
     OLED.setRotation(2);
   } else {
     OLED.setRotation(0);
   }
+  float minV = 3.0;
+  float maxV = 4.2;
+  float voltage = analogRead(A0) * (3.30 / 1023.00) * 5;
+  int percentage = 100 * (voltage - minV) / (maxV - minV);
   OLED.clearDisplay();
   OLED.setTextColor(SH110X_WHITE);
   OLED.setCursor(0, 0);
+  OLED.setTextSize(1);
+  OLED.print(percentage);
+  OLED.println("%\n");
   OLED.setTextSize(2);
-  OLED.print("x = ");
-  OLED.println(valx);
-  OLED.print("y = ");
-  OLED.println(valy);
-  OLED.print("z = ");
-  OLED.println(valz);
-  OLED.println(isLevel(valx, valy, valz) ? "Level!" : "Not Level");
+  OLED.println(yall);
+  OLED.println(isLevel(roll, pitch, yall) ? "Level!" : "Not Level");
 
   OLED.display();
 }
 
-void DisplayPortrait(int valx, int valy, int valz, bool flip) {
+void DisplayPortrait(float roll, float pitch, float yall, bool flip) {
   if (flip) {
     OLED.setRotation(1);
   } else {
@@ -76,19 +80,34 @@ void DisplayPortrait(int valx, int valy, int valz, bool flip) {
   OLED.clearDisplay();
   OLED.setTextColor(SH110X_WHITE);
   OLED.setCursor(0, 0);
-  OLED.setTextSize(2);
-  OLED.print("x ");
-  OLED.println(valx);
-  OLED.println();
-  OLED.print("y ");
-  OLED.println(valy);
-  OLED.println();
-  OLED.print("z ");
-  OLED.println(valz);
-  OLED.println();
-  OLED.println(isLevel(valx, valy, valz) ? "Level" : "Not\nLevel");
+  OLED.setTextSize(1);
+  OLED.println("roll \n");
+  OLED.println(roll);
+  OLED.println("pitch \n");
+  OLED.println(pitch);
+  OLED.println("yall \n");
+  OLED.println(yall);
+  OLED.println(isLevel(roll, pitch, yall) ? "Level" : "NotLevel");
 
   OLED.display();
+}
+
+void Display(float roll, float pitch, float yall, int orentation) {
+
+  switch (orentation) {
+    case 0:
+      DisplayLandscape(roll, pitch, yall, false);
+      break;
+    case 1:
+      DisplayPortrait(roll, pitch, yall, false);
+      break;
+    case 2:
+      DisplayLandscape(roll, pitch, yall, true);
+      break;
+    case 3:
+      DisplayPortrait(roll, pitch, yall, true);
+      break;
+  }
 }
 
 void setup() {
@@ -100,8 +119,46 @@ void setup() {
   Serial.println(mpu.begin() ? "MPU: Connected" : "MPU: Connection failed");
   Serial.println("Initialize OLED");
   Serial.println(OLED.begin() ? "Screen: Connected" : "Screen: Connection failed");
-  
-  // myButton.begin();
+}
+
+// Analog input
+int analoginput() {
+  int analogValue = analogRead(A0);
+  float voltage = analogValue * (3.30 / 1023.00);
+
+  if (voltage >= 3.0) {
+    // Serial.println("Button 4");
+    return 4;
+  } else if (voltage >= 2.5) {
+    // Serial.println("Button 3");
+    return 3;
+  } else if (voltage >= 2.0) {
+    // Serial.println("Button 2");
+    return 2;
+  } else if (voltage >= 1.5) {
+    // Serial.println("Button 1");
+    return 1;
+  } else {
+    Serial.print("Battery Voltage: ");
+    Serial.println(voltage * 5);
+    return 0;
+  }
+}
+
+int buttoninput() {
+  // do {
+  int buttonRead = analoginput();
+  if (buttonRead = 0) {
+    buttonstate = buttonRead;
+    // break;
+  } else if (buttonstate != buttonRead) {
+    buttonstate = buttonRead;
+    Serial.println("buttonstate = " + buttonstate);
+    // break;
+  }
+  delay(100);
+  // } while (1);
+  return buttonstate;
 }
 
 void loop() {
@@ -109,37 +166,25 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  float roll  = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI;
+  float roll = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI;
   float pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-  float yaw   = atan2(a.acceleration.z, a.acceleration.x) * 180.0 / PI;
+  float yaw = atan2(a.acceleration.z, a.acceleration.x) * 180.0 / PI;
 
-  float valx = roll;
-  float valy = pitch;
-  float valz = yaw;
+  //buttoninput();
+  Serial.println(analogRead(A0) * 3.30 / 1023.00);
 
-  Serial.print(" axis x = ");
-  Serial.print(valx);
-  Serial.print(" axis y = ");
-  Serial.print(valy);
-  Serial.print(" axis z = ");
-  Serial.print(valz);
+  tone(buzzerPin, 4000);
 
-  int orentation = 0;
+  // Serial.print(" axis x = ");
+  // Serial.print(valx);
+  // Serial.print(" axis y = ");
+  // Serial.print(valy);
+  // Serial.print(" axis z = ");
+  // Serial.print(valz);
 
-  switch (orentation) {
-    case 0:
-      DisplayLandscape(valx, valy, valz, false);
-      break;
-    case 1:
-      DisplayPortrait(valx, valy, valz, false);
-      break;
-      case 2:
-      DisplayLandscape(valx, valy, valz, true);
-      break;
-    case 3:
-      DisplayPortrait(valx, valy, valz, true);
-      break; 
-  }
+  Display(roll, pitch, roll, orentation % 5);
+
+
 
   delay(1000);
 }
